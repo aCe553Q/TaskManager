7b6d738a5c3497e137e593be87d1b0a63758f192
aCe553Q <135604004+aCe553Q@users.noreply.github.com>
2025-08-30 17:59:16 +0200

    Refactor AppDbContext, relationships and properties

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,11 +3,27 @@ using TaskManager.Models;
 
 namespace TaskManager.Data
 {
-   public class AppDbContext : DbContext
+    public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
         public DbSet<Board> Boards => Set<Board>();
         public DbSet<Column> Columns => Set<Column>();
         public DbSet<Card> Cards => Set<Card>();
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Board>()
+                .HasMany(b => b.Columns)
+                .WithOne(c => c.Board)
+                .HasForeignKey(c => c.BoardId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Column>()
+                .HasMany(c => c.Cards)
+                .WithOne(ca => ca.Column)
+                .HasForeignKey(ca => ca.ColumnId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
